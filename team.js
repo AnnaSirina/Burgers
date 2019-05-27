4acca2567ba1d1f821fbb103160d30e600510f54
Sirina Anna <sirina.anya@gmail.com>
2019-05-27 23:29:14 +0300

    выпадающее меню команда

--- team.js
+++ team.js
@@ -1,24 +1,59 @@
-const title = document.getElementsByClassName('team__item'),
+const el = document.getElementsByClassName('team__item'),
     cont = document.getElementsByClassName('team__hidden');
 
-    for (var i = 0; i < title.length; i++) {
-            title[i].addEventListener('click', function() {
+    for (var i = 0; i < el.length; i++) {
+            el[i].addEventListener('click', function() {
                  if (!(this.classList.contains('active'))) {
 
-                     for(var i = 0; i < title.length; i++) {
-                        title[i].classList.remove('active');
+                     for(var i = 0; i < el.length; i++) {
+                        el[i].classList.remove('active');
+                         if(this.children.length>1) {
+                          this.children[1].style.display = "0";
+                           this.children[1].style.overflow = "hidden"; 
+                        } else {
+                           return false;
+                        }
+
                     }
                     this.classList.add('active');
+                    for(var i=0; i<el.length; i++) { 
+                    if(this.children.length>1) {
+                          this.children[1].style.height = "auto";
+                          this.children[1].style.overflow = "visible";
+                          
+                       } else {
+                          return false;
+                       }
+                      
                     
-                    if (this.classList.contains('active')) {
-                        for (var j = 0; j < cont.length; j++) 
-                        cont[j].classList.add('active');
-                    } else {
-                        cont[j].classList.remove('active');
-                    }
-                
-                
          }
     
-            })
+            }
+        })
         }
+
+/*
+
+for(var i=0; i<el.length; i++) {
+ el[i].addEventListener("mouseenter", showSub, false);
+ el[i].addEventListener("mouseleave", hideSub, false);
+    
+    function showSub(e) {
+   if(this.children.length>1) {
+      this.children[1].style.height = "auto";
+      this.children[1].style.overflow = "visible";
+      this.children[1].style.opacity = "1";
+   } else {
+      return false;
+   }
+}
+function hideSub(e) {
+    if(this.children.length>1) {
+      this.children[1].style.height = "0px";
+       this.children[1].style.overflow = "hidden";
+       this.children[1].style.opacity = "0";
+    } else {
+       return false;
+    }
+}
+}*/
